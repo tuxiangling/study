500a56c6209de4e3a8073951356b2e37bcdb321c
tuge <15638089075@163.com>
2025-04-10 14:15:06 +0800

    Changes

--- src/Main.java
+++ src/Main.java
@@ -498,20 +498,20 @@ import java.util.Scanner;
 //        }
 
         //函数内的变量 - 本地变量
-        public class Main{
-            public static int factor(int i) {
-
-                if (i == 1) {
-                    return i;
-                }
-                return i * factor(i - 1);
-
-            }
-
-            public static void main(String[] args){
-                Scanner in = new Scanner(System.in);
-                int k = factor(3);
-                System.out.println(k);
+//        public class Main{
+//            public static int factor(int i) {
+//
+//                if (i == 1) {
+//                    return i;
+//                }
+//                return i * factor(i - 1);
+//
+//            }
+//
+//            public static void main(String[] args){
+//                Scanner in = new Scanner(System.in);
+//                int k = factor(3);
+//                System.out.println(k);
 
 
 //本地变量规则
@@ -519,17 +519,16 @@ import java.util.Scanner;
         //它也可以是定义在函数的块内
         //也可以定义在语句的块内
         //甚至可以随便拉一对大括号来定义
-                {
-                    int hh = 0;
-                    System.out.println(hh);
-                }
+//                {
+//                    int hh = 0;
+//                    System.out.println(hh);
+//                }
                 //快外面定义的变量在里面依然有效
                 //不能在同一个块内定义同名的变量，也不能定义块外面定义过的变量
                 //本地变量不会被默认初始化
 //程序运行进入这个快之前，其中的变量不存在，离开这个块，其中的变量就消失了
-                }
-                }
-
+//                }
+//                }
 
 
 
